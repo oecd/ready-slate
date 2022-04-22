0ac685ca99b4b34b38825152bf9cc5f229704fe3
Bertrand Riviere <bertrand.riviere@gmail.com>
2022-04-22 15:48:08 +0200

    minor css changes

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require('./package.json');
 
@@ -22,9 +21,8 @@ export default [
         sourcemap: true,
       },
     ],
-    external: [/@babel\/runtime/],
+    external: [/@babel\/runtime/, /prop-types/, /react/],
     plugins: [
-      peerDepsExternal(),
       resolve(),
       babel({
         exclude: 'node_modules/**',
