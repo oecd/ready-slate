e19670a72c93475520e76a3610e781e9ee350f85
Bertrand Riviere <bertrand.riviere@gmail.com>
2024-03-21 11:00:35 +0100

    deploy demo site

--- _next/static/6lsIvuYpB0vW3rOi-LCh0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/909-96f55c2ab5d750c2.js","static/chunks/pages/index-cd3a98567b4096c8.js"],"/_error":["static/chunks/pages/_error-dffd65023f03470c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
